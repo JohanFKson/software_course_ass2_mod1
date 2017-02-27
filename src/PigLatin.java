f3f5a643f8c618d7f46f17997f7c9f5855339e23
Fredrik Hagebring <fredrik@hagebring.com>
2017-02-27 11:06:29 +0100

    Fixed translateSentence using a dummy version of translateWord.

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,21 +1,37 @@
+import java.util.regex.*;
 public class PigLatin {
 
-	public static String translate(String word) {
-		return "test";
-	}
-
 	public static String translateWord(String word) {
-		return "test2";
+		return word+"test";
 	}
 
 	public static String translateSentence(String sentence) {
-		return "test2";
+
+		PigLatin translator = new PigLatin();
+		String translation = "";
+
+		String regex = "([a-zA-Z]*)([^a-zA-Z]*)";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(sentence);
+
+
+        String word, separator;
+        while(matcher.find()) {
+        	word = matcher.group(1);
+        	separator = matcher.group(2);
+        	if (word.length() > 0)
+        		translation += translator.translateWord(word);
+        	translation += separator;
+        }
+
+		return translation;
 	}
 
 	public static void main(String[] args) {
-		System.out.print("Please enter the word you want to translate: ");
-		String word = "test"; // System.console().readLine();
-		String translation = translate(word);
+		System.out.print("Please enter the sentence you want to translate: ");
+		String sentence = System.console().readLine();
+		String translation = translateSentence(sentence);
 		System.out.println("Translation: " + translation);
 	}
 }	
--- src/PigLatinTest.java
+++ src/PigLatinTest.java
@@ -2,13 +2,23 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class PigLatinTest {
-  @Test
-  public void evaluatesExpression() {
 
-    PigLatin translator = new PigLatin();
+	@Test
+	public void testTranslateSentence() {
+		PigLatin t = new PigLatin();
 
-    String word = "test";
-    String translation = translator.translate(word);
-    assertEquals("test", translation);
-  }
+		String sentence = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.";
+		
+		//return String.join(" ", words);
+		String translation = t.translateSentence(sentence);
+		String truth = t.translateWord("Lorem") + " " +
+					t.translateWord("ipsum") + " " +
+					t.translateWord("dolor") + " " +
+					t.translateWord("sit") + " " +
+					t.translateWord("amet") + ", " +
+					t.translateWord("consectetuer") + " " +
+					t.translateWord("adipiscing") + " " +
+					t.translateWord("elit") + ".";
+		assertEquals(translation, truth);
+	}
 }
