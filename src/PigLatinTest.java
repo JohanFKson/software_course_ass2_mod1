cff0223e2ed66bf5543c5a921e9b6272fe76fb1f
Johan Karlsson <jokarls@net.chalmers.se>
2017-03-10 09:22:48 +0100

    Startmenu

--- src/PigLatinTest.java
+++ src/PigLatinTest.java
@@ -29,32 +29,48 @@ public class PigLatinTest {
 		String[] english_words = new String[]{"pig", "Latin", "cheers", "smile", "eat", "omelet"};
 		String[] piglatin_words = new String[]{"igpay", "Atinlay", "eerschay", "ilesmay", "eatway", "omeletway"};
 
-		for(int i = 0; i < english_words.length; ++i) {
+		for (int i = 0; i < english_words.length; ++i) {
 			String piglatin_word = t.translateWord(english_words[i]);
 			assertEquals(piglatin_words[i], piglatin_word);
 		}
 	}
 
-	@Test
+@Test
 	public void testChoice() {
 		PigLatin t = new PigLatin();
 
-		String choice1="1";
-		String testPrint1="Please enter the sentence you want to translate: ";
-		String printVariable1=t.executeChoice(choice1);
-		assertEquals(testPrint1,printVariable1);
+		String choice1 = "1";
+		String testPrint1 = "Please enter the sentence you want to translate: ";
+		String printVariable1 = t.executeChoice(choice1);
+		assertEquals(testPrint1, printVariable1);
 
 
-		String choice2="2";
-		String testPrint2="A word will be generated at random";
-		String printVariable2=t.executeChoice(choice2);
-		assertEquals(testPrint2,printVariable2);
+		String choice2 = "2";
+		String testPrint2 = "Translate the following word into piglatin: ";
+		String printVariable2 = t.executeChoice(choice2);
+		assertEquals(testPrint2, printVariable2);
 
 
-		String choice3="1+2";
-		String testPrint3="Not a valid choice. The program will terminate.";
-		String printVariable3=t.executeChoice(choice3);
-		assertEquals(testPrint3,printVariable3);
+		String choice3 = "1+2";
+		String testPrint3 = "Not a valid choice!";
+		String printVariable3 = t.executeChoice(choice3);
+		assertEquals(testPrint3, printVariable3);
+		}
 
-	}
+@Test
+   public void testpracticeMode() {
+	PigLatin t = new PigLatin();
+
+	String pigLatinGuess="pig";
+	String pigLatinAnswer="igpay";
+
+	String practiceResult=t.verifyAnswer(pigLatinGuess,pigLatinAnswer);
+	assertEquals(practiceResult,"Wrong! You guessed: pig Correct answer: igpay");
+
+	pigLatinGuess="igpay";
+	pigLatinAnswer="igpay";
+
+	practiceResult=t.verifyAnswer(pigLatinGuess,pigLatinAnswer);
+	assertEquals(practiceResult,"Correct!");
+    }
 }
